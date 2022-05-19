4261a43a4515739c966b08f1139528731ce4ea1c
qianjiachun <189964430@qq.com>
2022-05-19 21:15:34 +0800

    init

--- typing.d.ts
+++ typing.d.ts
@@ -7,3 +7,8 @@ type IMsgHandler = (data: string) => void;
  * @param {any} error Error from websocket
  */
 type IErrorHandler = (error: any) => void;
+
+class IDouyuDanmu {
+    constructor(rid: string, msgHandler: IMsgHandler, closeHandler?: IErrorHandler);
+    close(): void;
+}
